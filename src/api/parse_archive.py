6fba9475986b9814be41754ca96af68991eb4678
niikage <lifernod@gmail.com>
2026-06-14 22:18:05 +0700

    Заглушка для ответа на вопрос (/api/ask)

--- src/api/parse_archive.py
+++ src/api/parse_archive.py
@@ -1,12 +1,8 @@
 import asyncio
-import os
-from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 from .process_file import process_file
 
-MIN_FILES_FOR_PARALLEL = 20
-POOL = ProcessPoolExecutor(max_workers=os.cpu_count())
 
 async def process_archive(extract_dir: Path) -> list[bool]:
     """
@@ -22,19 +18,11 @@ async def process_archive(extract_dir: Path) -> list[bool]:
         if not should_skip(p)
     ]
 
-    if len(py_files) >= MIN_FILES_FOR_PARALLEL:
-        futures = [
-            loop.run_in_executor(
-                POOL,
-                process_file,
-                path
-            )
-            for path in py_files
-        ]
-
-        return await asyncio.gather(*futures)
-    else:
-        return [process_file(f) for f in py_files]
+    # TODO:
+    # chunks = await asyncio.gather(*futures)
+    # write_to_database(chunks)
+    # return ???
+    return [process_file(f) for f in py_files]
 
 #########################################################################
 ## Utils
