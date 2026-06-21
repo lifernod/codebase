1b8ab220d2b60cf2fca210a6c21f426c9e40782b
lovelessunloveable <lovelessunloveable@gmail.com>
2026-06-21 20:55:56 +0700

    Changed style.css, added metrics.py, changed inconsistencies in design between two frontend developers.

--- src/frontend/utils.py
+++ src/frontend/utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def load_css(file_name="style.css"):
-    with open(file_name, "r") as f:
+    with open(file_name, "r", encoding="utf-8") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
@@ -20,7 +20,7 @@ def render_sidebar():
                 st.switch_page(page)
 
         nav_btn("Чат", chat_svg, "pages/chat.py")
-        nav_btn("Метрики", metrics_svg, "/")
+        nav_btn("Метрики", metrics_svg, "pages/metrics.py")
         nav_btn("Архивы", archives_svg, "pages/archive.py")
 
         st.markdown('</div>', unsafe_allow_html=True)
